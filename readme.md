43dc158da1f4241c2a0722e236ee55f082f6bb99
Alfonso Catron <alfonso.catron@gmail.com>
2017-08-18 14:53:21 -0300

    Added sidebar areas

--- readme.md
+++ readme.md
@@ -7,3 +7,4 @@ I'll be updating this readme.md with my steps and a timestamp each update (will 
 - 13:47 - Wordpress local install, .gitignore and readme.md creation
 - 14.12 - Created theme folder and barebones theme files: index.php, header.php, footer.php, style.css, screenshot.png and functions.php 
 - 14.45 - Updated repo with all WP files. Spent some time reducing errors at Theme Check 
+- 14.53 - Added theme sidebars
--- wp-content/themes/deviget/functions.php
+++ wp-content/themes/deviget/functions.php
@@ -2,9 +2,59 @@
 
 
 // add title-tag support
-
 add_action( 'after_setup_theme', 'deviget_slug_setup' );
 
 function deviget_slug_setup() {
+	
 	add_theme_support( 'title-tag' );
-}
+
+} // title tag-suport end
+
+
+
+// theme sidebars
+if ( function_exists('register_sidebar') ) {
+
+	// main content sidebar
+   	register_sidebar(array(
+	   'name' => 'Sidebar',
+	   'id' => 'sidebar-1',  
+	   'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	   'after_widget' => '</div>',
+	   'before_title' => '<h2>',
+	   'after_title' => '</h2>'
+    ));
+
+ 	// Footer sidebars
+  	register_sidebar(array(
+	   'name' => 'Footer  1',
+	   'id' => 'sidebar-footer-1',  
+	   'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	   'after_widget' => '</div>',
+	   'before_title' => '<h2>',
+	   'after_title' => '</h2>'
+   	));
+
+   	register_sidebar(array(
+	   'name' => 'Footer 2',
+	   'id' => 'sidebar-footer-2',  
+	   'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	   'after_widget' => '</div>',
+	   'before_title' => '<h2>',
+	   'after_title' => '</h2>'
+   	));
+
+   	register_sidebar(array(
+	   'name' => 'Footer 3',
+	   'id' => 'sidebar-footer-3',  
+	   'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	   'after_widget' => '</div>',
+	   'before_title' => '<h2>',
+	   'after_title' => '</h2>'
+   	));
+
+} // theme sidebars end
+
+
+
+?>
